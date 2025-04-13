8d5a47497ed4fbfd2df95438e8376ab78aecc777
Noman Mia <nomanislamnishad@gmail.com>
2025-04-13 12:14:03 +0600

    frist step done

--- app/Http/Controllers/SSLCommerzCredentialController.php
+++ app/Http/Controllers/SSLCommerzCredentialController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SSLCommerzCredential;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -12,7 +13,10 @@ class SSLCommerzCredentialController extends Controller
      */
     public function index()
     {
-        return Inertia::render('Settings/Settings');
+        $setting = SSLCommerzCredential::first();
+        return Inertia::render('Settings/Settings', [
+            'setting' => $setting,
+        ]);
     }
 
     /**
@@ -52,7 +56,25 @@ class SSLCommerzCredentialController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        try {
+            $request->validate([
+                'store_id' => 'required|string',
+                'store_password' => 'required|string',
+                'currency' => 'required|string',
+                'success_url' => 'required|string',
+                'fail_url' => 'required|string',
+                'cancel_url' => 'required|string',
+                'ipn_url' => 'required|string',
+                'init_url' => 'required|string',
+            ]);
+
+            $setting = SSLCommerzCredential::first();
+            $setting->update($request->all());
+            return redirect()->back()->with('success', 'SSLCommerz settings updated successfully.');
+            
+        } catch (\Throwable $th) {
+            return redirect()->back()->with('error', $th->getMessage());
+        }
     }
 
     /**
