8434c407c3ce71aa760e0ad058d42753ed03a992
Noman Mia <nomanislamnishad@gmail.com>
2025-04-16 20:06:46 +0600

    added brands front end setup

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,12 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\SSLCommerzCredentialController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BrandController;
 
+Route::redirect("/", "/dashboard");
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('page.dashboard');
-Route::resource('/settings',SSLCommerzCredentialController::class);
 
+Route::resource('/settings',SSLCommerzCredentialController::class);
+Route::resource('/brands', BrandController::class);
 Route::get('/login', [AuthController::class,'loginPage'])->name('login');
 Route::post('/login', [AuthController::class,'login'])->name('login.post');
